6672a1dc8f3e44da19b5104f2570ddea78392a5f
swatipatil-webonise <55143985+swatipatil-webonise@users.noreply.github.com>
2019-10-09 22:11:29 +0530

    Update App.js

--- todooperation/src/App.js
+++ todooperation/src/App.js
@@ -5,7 +5,6 @@ import { Todo } from './Todo';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-
 class App extends React.Component {
   render() {
     return (
