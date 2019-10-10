41fe889ce45c1c97ad29de06749223a92587dfa6
swati <swati.patil@weboniselab.com>
2019-10-10 21:17:21 +0530

    add Todo.js with proper single quotes

--- todoappWithRedux/src/Todo.js
+++ todoappWithRedux/src/Todo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { AddTodo } from './AddTodo';
 import { ListTodo } from './ListTodo';
-import { connect } from "react-redux";
-import { addTodo , deleteTodo , updateTodo} from "./store/actions/todo";
+import { connect } from 'react-redux';
+import { addTodo , deleteTodo , updateTodo} from './store/actions/todo';
 
 class Todo extends React.Component {
   constructor(props) {
